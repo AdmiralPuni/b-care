0f04188361cf4a622813471dc672508d46219345
AdmiralPuni <admiralpuni@protonmail.com>
2022-05-23 20:08:58 +0700

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -61,7 +61,7 @@
                             </div>
                         </div>
                         <div class="container-fluid p-2 bg-white shadow mb-3">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            <img class="img-fluid" src="https://pbs.twimg.com/profile_images/798555501067837440/HF8_0JeX_400x400.jpg" alt="">
                         </div>
                     </div>
                 </div>
